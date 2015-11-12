2d03f4236a91bf3195261b520c02ba0dd22dca08
mike vallano <mikevallano@gmail.com>
2015-11-12 16:41:36 -0600

    Modified SID

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -2,6 +2,7 @@ class ServiceController < ApplicationController
 
   def agent
     @customer = Customer.first
+    #note: this needs to be changed due to the account sid
     capability = Twilio::Util::Capability.new(ENV["twilio_account_sid"], ENV["twilio_auth_token"])
     capability.allow_client_outgoing(ENV["twilio_twiml_app_sid"])
     @token = capability.generate()
@@ -9,8 +10,8 @@ class ServiceController < ApplicationController
 
   def streetsign
     @street = Street.first
-    capability = Twilio::Util::Capability.new(ENV["twilio_street_account_sid"], ENV["twilio_auth_token"])
-    capability.allow_client_outgoing(ENV["twilio_street_account_sid"])
+    capability = Twilio::Util::Capability.new(ENV["twilio_account_sid"], ENV["twilio_auth_token"])
+    capability.allow_client_outgoing(ENV["twilio_account_sid"])
     @token = capability.generate()
   end
 end
