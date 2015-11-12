44b00fa77f8ce8406562c13ce13de9da8e3eb84e
mike vallano <mikevallano@gmail.com>
2015-11-12 16:37:43 -0600

    Updated SID for street twilio app

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -9,8 +9,8 @@ class ServiceController < ApplicationController
 
   def streetsign
     @street = Street.first
-    capability = Twilio::Util::Capability.new(ENV["twilio_account_sid"], ENV["twilio_auth_token"])
-    capability.allow_client_outgoing(ENV["twilio_twiml_app_sid"])
+    capability = Twilio::Util::Capability.new(ENV["twilio_street_account_sid"], ENV["twilio_auth_token"])
+    capability.allow_client_outgoing(ENV["twilio_street_account_sid"])
     @token = capability.generate()
   end
 end
