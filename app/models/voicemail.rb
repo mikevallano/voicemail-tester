df0a60adf0540e07be59b6219ea166a5a0128fb4
mike vallano <mikevallano@gmail.com>
2015-11-13 14:39:59 -0600

    Modified by_street method

--- app/models/voicemail.rb
+++ app/models/voicemail.rb
@@ -1,5 +1,5 @@
 class Voicemail < ActiveRecord::Base
   belongs_to :street
 
-  scope :by_street, lambda { |user| where(:street_id => street.id) }
+  scope :by_street, lambda { |street| where(:street_id => street.id) }
 end
